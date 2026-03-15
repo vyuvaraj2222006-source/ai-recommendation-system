725a516133f3fc541b42522a9c9a0abcfb2025f3
YUVARAJ.V <v.yuvaraj.22.2.2006@gmail.com>
2026-03-15 20:45:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -739,8 +739,6 @@ def track_interaction():
         
     except Exception as e:
         return jsonify({"error": str(e)}), 400
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
-app.run(host="0.0.0.0", port=10000)
